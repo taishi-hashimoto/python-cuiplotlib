bea406d54cad3204c56be8e66115cb888442d3af
Taishi Hashimoto <hashimoto.taishi@outlook.com>
2024-08-02 00:50:36 +0900

    feat: Axes class

--- src/cuiplotlib/examples/line.py
+++ src/cuiplotlib/examples/line.py
@@ -1,23 +1,26 @@
 import curses
 import numpy as np
 import time
-from ..line import line
+from ..line import Axes
 
 
 def window(stdscr: curses.window):
     curses.curs_set(0)
     stdscr.nodelay(True)
 
-    ymax, xmax = stdscr.getmaxyx()
-
+    axes = Axes(stdscr, 5, 1, 0, 0)
+    axes._y0 = 0
     
+    off = 0
     while True:
         try:
+            stdscr.clear()
+            off += 2*np.pi / 100
             x = np.linspace(0, 2*np.pi)
-            y = np.sin(x)
-            line(stdscr, 10, 2, xmax//2, ymax-5, x, y)
+            y = np.sin(x - off)
+            axes.line(x, y)
             stdscr.refresh()
-            time.sleep(1)
+            time.sleep(0.1)
         except KeyboardInterrupt:
              break
         
--- src/cuiplotlib/line.py
+++ src/cuiplotlib/line.py
@@ -41,44 +41,109 @@ class Transform:
 
     def __call__(self, x, y):
         xx = (x - self._xmin) / (self._xmax - self._xmin) * self._width + self._left
-        yy = (y - self._ymin) / (self._ymax - self._ymin) * -self._height + self._top + self._height
+        yy = (y - self._ymin) / (self._ymax - self._ymin) * -self._height + self._top + self._height - 0.5
         return xx, yy
 
 
-def line(
-    window: curses.window,
-    x0, y0,
-    width,
-    height, 
-    x, y,
-    ha="left",
-    va="top"
-):
-    ymax, xmax = window.getmaxyx()
-    x0, y0 = compute_position(xmax, ymax, x0, y0, ha=ha, va=va, width=width, height=height)
+class Axes:
+    def __init__(
+        self,
+        window: curses.window,
+        left_margin=0, bottom_margin=0,
+        right_margin=0, top_margin=0
+    ):
+        ymax, xmax = window.getmaxyx()
+        width = xmax - left_margin - right_margin
+        height = ymax - bottom_margin - top_margin
+        bottom_margin += 1
+        width -= 1
+        height -= 1
+        self._window = window
+        self._top = ymax - bottom_margin - height
+        self._left = left_margin
+        self._width = width
+        self._height = height
+        self._xmin = None
+        self._xmax = None
+        self._ymin = None
+        self._ymax = None
+        self._transform = None
+        self._x0 = None
+        self._y0 = None
 
-    transform = Transform(y0, x0, height, width, 0, 2*np.pi, -1, 1)
-    # Axes
-    xaxis_x, xaxis_y = transform(np.linspace(0, 2*np.pi, 2000), 0)
-    yaxis_x, yaxis_y = transform(0, np.linspace(-1, 1, 2000))
-    for x1 in xaxis_x:
-        window.addstr(int(xaxis_y), int(x1), "_")
-    for y1 in yaxis_y:
-        window.addstr(int(y1), int(yaxis_x), "|")
-    window.addstr(int(xaxis_y), int(yaxis_x), "|")
-    window.addstr(ymax-1, xmax-2, "o")
+    def _set_transform(self, xmin=None, ymin=None, xmax=None, ymax=None):
+        if xmin is not None and self._xmin is None:
+            self._xmin = xmin
+        if xmax is not None and self._xmax is None:
+            self._xmax = xmax
+        if ymin is not None and self._ymin is None:
+            self._ymin = ymin
+        if ymax is not None and self._ymax is None:
+            self._ymax = ymax
+        self._transform = Transform(
+            self._top, self._left, self._height, self._width,
+            self._xmin, self._xmax, self._ymin, self._ymax)
 
-    wf, hf = transform(x, y)
-    # raise ValueError(f"{xx}, {yy}")
-    # xx, yy = compute_position(xmax-1, ymax-1, xx, yy, xmax-2, ymax-2)
-    # np.interp(np.arange())
-    # raise ValueError(f"{np.around((hf - np.floor(hf)), 1)}, {hf}")
-    xx = np.linspace(x0, x0+width, width)
-    yy = np.interp(xx, wf, hf)
-    
-    yy0 = np.ceil(yy)
-    cc = np.select((
-        yy0 - yy > 2/3,
-        yy0 - yy > 1/3), "`-", ".")
-    for xi, yi, c in zip(xx, yy, cc):
-            window.addstr(math.ceil(yi), math.floor(xi), c)
+    def set_xlim(self, xmin=None, xmax=None):
+        if xmin is not None:
+            self._xmin = xmin
+        if xmax is not None:
+            self._xmax = xmax
+        self._set_transform()
+
+    def set_ylim(self, ymin=None, ymax=None):
+        if ymin is not None:
+            self._ymin = ymin
+        if ymax is not None:
+            self._ymax = ymax
+        self._set_transform()
+
+    def axes(
+        self,
+    ):
+        # Axes
+        if self._x0 is None:
+            x0 = self._xmin
+        else:
+            x0 = self._x0
+        if self._y0 is None:
+            y0 = self._ymin
+        else:
+            y0 = self._y0
+        xaxis_x, xaxis_y = self._transform((self._xmin, self._xmax), y0)
+        yaxis_x, yaxis_y = self._transform(x0, (self._ymin, self._ymax))
+
+        xmin, xmax = np.fix(xaxis_x)
+        for x1 in np.arange(xmin, xmax+1, 1):
+            try:
+                self._window.addstr(math.ceil(xaxis_y), int(x1), "_")
+            except:
+                pass
+        ymax, ymin = np.ceil(yaxis_y)
+        for y1 in  np.arange(ymin, ymax+1, 1):
+            try:
+                self._window.addstr(math.ceil(y1), int(yaxis_x), "|")
+            except:
+                pass
+
+    def line(
+        self,
+        x, y
+    ):
+        self._set_transform(np.min(x), np.min(y), np.max(x), np.max(y))
+        
+        self.axes()
+
+        wf, hf = self._transform(x, y)
+        xx = np.linspace(self._left, self._left+self._width, self._width)
+        yy = np.interp(xx, wf, hf)
+
+        yy0 = np.ceil(yy)
+        cc = np.select((
+            yy0 - yy > 2/3,
+            yy0 - yy > 1/3), "`-", ".")
+        for xi, yi, c in zip(xx, yy, cc):
+            try:
+                self._window.addstr(math.ceil(yi), math.floor(xi), c)
+            except:
+                pass
